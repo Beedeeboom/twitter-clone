12889bf0c8189e4ee9df9202ed9b7a4786716c6d
Beedeeboom <stephanie.duffieux@gmail.com>
2020-05-03 22:49:43 +1000

    added scaffold for Twites

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,21 @@
 class ApplicationController < ActionController::Base
     before_action :authenticate_user!
 
+    before_action :check_user_profile, except: [:edit]
+
+    def check_user_profile()
+        if user_signed_in?
+            
+            if current_user.name == nil
+                redirect_to edit_user_path(current_user)
+            elsif current_user.address == nil
+                redirect_to edit_user_path(current_user) 
+            elsif current_user.display_name == nil
+                redirect_to edit_user_path(current_user)
+            end
+        end
+    end
+
     def after_sign_in_path_for(resource)
         return new_user_path
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
-
-  def index
-    @users = User.all
-  end
-
   def show
   end
 
@@ -34,7 +29,7 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.html { redirect_to root, notice: 'User was successfully updated.' }
         format.json { render :show, status: :ok, location: @user }
       else
         format.html { render :edit }
